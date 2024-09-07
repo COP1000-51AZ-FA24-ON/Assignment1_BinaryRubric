24ff41bae1328016a97a012cfb89e138e4b9fe30
gulfalaska <153951625+gulfalaska@users.noreply.github.com>
2024-09-06 21:18:52 -0400

    Update withbinaryrubic.py toDecimal

--- withbinaryrubic.py
+++ withbinaryrubic.py
@@ -1,6 +1,6 @@
 from binaryconvert import convert
 
-def binaryToDecimal(n):
+def toDecimal(n):
         return int(n,2)
 
 def toBinary(text, bit):
@@ -11,18 +11,18 @@ def toBinary(text, bit):
         return ' '.join(binary_list)
 
 print("SirVon Andre Thomas")
-print("S =" ,toBinary("S",8),"=",binaryToDecimal(convert.ToBinary("S",8)))
-print("i =" ,toBinary("i",8),"=",binaryToDecimal(convert.ToBinary("i",8)))
-print("r =" ,toBinary("r",8),"=",binaryToDecimal(convert.ToBinary("r",8)))
-print("V =" ,toBinary("V",8),"=",binaryToDecimal(convert.ToBinary("V",8)))
-print("o =" ,toBinary("o",8),"=",binaryToDecimal(convert.ToBinary("o",8)))
-print("n =" ,toBinary("n",8),"=",binaryToDecimal(convert.ToBinary("n",8)))
-print("A =" ,toBinary("A",8),"=",binaryToDecimal(convert.ToBinary("A",8)))
-print("T =" ,toBinary("T",8),"=",binaryToDecimal(convert.ToBinary("T",8)))
-print("h =" ,toBinary("h",8),"=",binaryToDecimal(convert.ToBinary("h",8)))
-print("o =" ,toBinary("o",8),"=",binaryToDecimal(convert.ToBinary("o",8)))
-print("m =" ,toBinary("m",8),"=",binaryToDecimal(convert.ToBinary("m",8)))
-print("a =" ,toBinary("a",8),"=",binaryToDecimal(convert.ToBinary("a",8)))
-print("s =" ,toBinary("s",8),"=",binaryToDecimal(convert.ToBinary("s",8)))
+print("S =" ,toBinary("S",8),"=",toDecimal(convert.ToBinary("S",8)))
+print("i =" ,toBinary("i",8),"=",toDecimal(convert.ToBinary("i",8)))
+print("r =" ,toBinary("r",8),"=",toDecimal(convert.ToBinary("r",8)))
+print("V =" ,toBinary("V",8),"=",toDecimal(convert.ToBinary("V",8)))
+print("o =" ,toBinary("o",8),"=",toDecimal(convert.ToBinary("o",8)))
+print("n =" ,toBinary("n",8),"=",toDecimal(convert.ToBinary("n",8)))
+print("A =" ,toBinary("A",8),"=",toDecimal(convert.ToBinary("A",8)))
+print("T =" ,toBinary("T",8),"=",toDecimal(convert.ToBinary("T",8)))
+print("h =" ,toBinary("h",8),"=",toDecimal(convert.ToBinary("h",8)))
+print("o =" ,toBinary("o",8),"=",toDecimal(convert.ToBinary("o",8)))
+print("m =" ,toBinary("m",8),"=",toDecimal(convert.ToBinary("m",8)))
+print("a =" ,toBinary("a",8),"=",toDecimal(convert.ToBinary("a",8)))
+print("s =" ,toBinary("s",8),"=",toDecimal(convert.ToBinary("s",8)))
 print("I like to code, travel and spend time with Family")
 print("I expect to understand the Python Language more from this class.")
